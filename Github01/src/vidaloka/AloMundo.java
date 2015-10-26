75fe2cc1be1ab4bdb52a2fc8b2b4d8629b0a55bb
J14 <joao.araujo.367@ig.com.br>
2015-10-26 17:09:16 -0200

    Primeiro commit

---
+++ Github01/src/vidaloka/AloMundo.java
@@ -0,0 +1,12 @@
+package vidaloka;
+
+public class AloMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Alo mundo do github!");
+
+	}
+
+}
